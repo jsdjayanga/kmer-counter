707bee915ee89b0f65e53a40195dd4aaea621c32
Jayanga Dissanayake <jsdjayanga@gmail.com>
2016-11-02 19:48:00 +0530

    Moving the iterative invoke to the kernel initiator

--- GPUHandler.cu
+++ GPUHandler.cu
@@ -2,111 +2,106 @@
 #include <inttypes.h>
 #include "GPUHandler.h"
 
-__global__ void bitEncode(char* input, char* filter, int64_t lineLength,
-		int64_t count) {
+__global__ void bitEncode(char* input, char* filter, int64_t lineLength) {
 	uint64_t index = (blockIdx.x * blockDim.x + threadIdx.x) * lineLength;
 
-	for (uint32_t localCount = 0; localCount < count; localCount++) {
+	uint64_t readValue = 0;
+	uint64_t filterValue = 0;
 
-		index += (lineLength * localCount);
+	uint16_t readValueLength = 0;
+	uint16_t filterValueLength = 0;
 
-		uint64_t readValue = 0;
-		uint64_t filterValue = 0;
+	int64_t i = index;
+	for (; i < index + lineLength; i++) {
+		if (i - index > 0 && (i - index) % 32 == 0) {
+			int64_t readValueLocation = ((((i - index) / 32) - 1)
+					* sizeof(int64_t)) + sizeof(int16_t) + index;
+			//int64_t filterLocation = ((((i - index) / 32) - 1) * sizeof (int64_t));
+			memcpy(&input[readValueLocation], &readValue, sizeof(int64_t));
+			//memcpy(&filter[filterLocation], &filterValue, sizeof (int64_t));
+			readValue = 0;
 
-		uint16_t readValueLength = 0;
-		uint16_t filterValueLength = 0;
-
-		int64_t i = index;
-		for (; i < index + lineLength; i++) {
-			if (i - index > 0 && (i - index) % 32 == 0) {
-				int64_t readValueLocation = ((((i - index) / 32) - 1)
-						* sizeof(int64_t)) + sizeof(int16_t) + index;
-				//int64_t filterLocation = ((((i - index) / 32) - 1) * sizeof (int64_t));
-				memcpy(&input[readValueLocation], &readValue, sizeof(int64_t));
-				//memcpy(&filter[filterLocation], &filterValue, sizeof (int64_t));
-				readValue = 0;
-
-				//filterValue = 0;
-			}
-
-			if (i > 0 && (i - index) % 64 == 0) {
-				int64_t filterLocation = ((((i - index) / 64) - 1)
-						* sizeof(int64_t)) + index;
-				memcpy(&filter[filterLocation], &filterValue, sizeof(int64_t));
-				filterValue = 0;
-			}
-
-			switch (input[i]) {
-			case 'A':
-				readValue <<= 2;
-				readValue |= 0;
-				readValueLength += 2;
-
-				filterValue <<= 1;
-				filterValueLength += 1;
-
-				continue;
-			case 'C':
-				readValue <<= 2;
-				readValue |= 1;
-				readValueLength += 2;
-
-				filterValue <<= 1;
-				filterValueLength += 1;
-
-				continue;
-			case 'G':
-				readValue <<= 2;
-				readValue |= 2;
-				readValueLength += 2;
-
-				filterValue <<= 1;
-				filterValueLength += 1;
-
-				continue;
-			case 'T':
-				readValue <<= 2;
-				readValue |= 3;
-				readValueLength += 2;
-
-				filterValue <<= 1;
-				filterValueLength += 1;
-
-				continue;
-			default:
-				readValue <<= 2;
-				readValue |= 3;
-				readValueLength += 2;
-
-				filterValue <<= 1;
-				filterValue |= 1;
-				filterValueLength += 1;
-				continue;
-			}
+			//filterValue = 0;
 		}
 
-		//printf("readValueLength==============:%"PRIu16"", readValueLength);
-		memcpy(&input[index], &readValueLength, sizeof(uint16_t));
-
-		if (i > 0 && (i - index) % 64 > 0) {
-			uint8_t shiftingReadValue = (32 - ((i - index) % 32)) * 2;
-			readValue <<= shiftingReadValue;
-
-			int64_t readValueLocation = ((((i - index) / 32)) * sizeof(int64_t))
-					+ sizeof(int16_t) + index;
-			memcpy(&input[readValueLocation], &readValue, sizeof(int64_t));
-			readValue = 0;
-			readValueLength = 0;
-
-			uint8_t shiftingFilterValue = 64 - ((i - index) % 64);
-			filterValue <<= shiftingFilterValue;
-			int64_t filterLocation = ((((i - index) / 64)) * sizeof(int64_t))
-					+ index;
+		if (i > 0 && (i - index) % 64 == 0) {
+			int64_t filterLocation =
+					((((i - index) / 64) - 1) * sizeof(int64_t)) + index;
 			memcpy(&filter[filterLocation], &filterValue, sizeof(int64_t));
 			filterValue = 0;
-			filterValueLength = 0;
+		}
+
+		switch (input[i]) {
+		case 'A':
+			readValue <<= 2;
+			readValue |= 0;
+			readValueLength += 2;
+
+			filterValue <<= 1;
+			filterValueLength += 1;
+
+			continue;
+		case 'C':
+			readValue <<= 2;
+			readValue |= 1;
+			readValueLength += 2;
+
+			filterValue <<= 1;
+			filterValueLength += 1;
+
+			continue;
+		case 'G':
+			readValue <<= 2;
+			readValue |= 2;
+			readValueLength += 2;
+
+			filterValue <<= 1;
+			filterValueLength += 1;
+
+			continue;
+		case 'T':
+			readValue <<= 2;
+			readValue |= 3;
+			readValueLength += 2;
+
+			filterValue <<= 1;
+			filterValueLength += 1;
+
+			continue;
+		default:
+			readValue <<= 2;
+			readValue |= 3;
+			readValueLength += 2;
+
+			filterValue <<= 1;
+			filterValue |= 1;
+			filterValueLength += 1;
+			continue;
 		}
 	}
+
+	//printf("readValueLength==============:%"PRIu16"", readValueLength);
+	memcpy(&input[index], &readValueLength, sizeof(uint16_t));
+
+	if (i > 0 && (i - index) % 64 > 0) {
+		uint8_t shiftingReadValue = (32 - ((i - index) % 32)) * 2;
+		readValue <<= shiftingReadValue;
+
+		int64_t readValueLocation = ((((i - index) / 32)) * sizeof(int64_t))
+				+ sizeof(int16_t) + index;
+		memcpy(&input[readValueLocation], &readValue, sizeof(int64_t));
+		readValue = 0;
+		readValueLength = 0;
+
+		uint8_t shiftingFilterValue = 64 - ((i - index) % 64);
+		filterValue <<= shiftingFilterValue;
+		int64_t filterLocation = ((((i - index) / 64)) * sizeof(int64_t))
+				+ index;
+		memcpy(&filter[filterLocation], &filterValue, sizeof(int64_t));
+		filterValue = 0;
+		filterValueLength = 0;
+	}
+
 }
 
 int64_t processKMers(const char* input, int64_t kmerLength, int64_t inputSize,
@@ -127,13 +122,16 @@ int64_t processKMers(const char* input, int64_t kmerLength, int64_t inputSize,
 	cudaMemset(d_output, 0, inputSize);
 	cudaMemset(d_filter, 0, inputSize / 2);
 
-	int32_t threadCount = 1024;
+	int32_t threadCount = 256;
 	int32_t count = inputSize / lineLength / threadCount;
 	if ((inputSize / lineLength) % threadCount > 0) {
 		count++;
 	}
-	bitEncode<<<1, threadCount>>>(d_input, d_filter, lineLength, count);
-	cudaDeviceSynchronize();
+
+	for (int32_t ite = 0; ite <= count; ite++) {
+		bitEncode<<<1, threadCount>>>(&d_input[threadCount * lineLength * ite], d_filter, lineLength);
+		cudaDeviceSynchronize();
+	}
 
 	if (debug == true) {
 		char* temp = new char[inputSize];
